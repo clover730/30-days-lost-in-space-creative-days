d8108d8c02ddc051fa8fb5267220430911d62888
clover730 <bridget.desai@gmail.com>
2024-08-06 10:27:07 -0400

    Add files via upload

---
+++ bridgetday5 (1).ino
@@ -0,0 +1,18 @@
+int ledPin = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (ledPin, HIGH);
+delay (1000);
+digitalWrite (ledPin, LOW);
+delay (1000);
+digitalWrite (ledPin, HIGH);
+delay (3500);
+digitalWrite (ledPin, LOW);
+delay (500);
+}
